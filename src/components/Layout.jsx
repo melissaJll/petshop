ca4d1426fb67309d08ea5e3c6b45369210936bd1
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-17 11:17:42 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,7 +18,7 @@ export default function Layout({ children }) {
           <Link href="/contato">Contato</Link>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className="limitador">{children}</main>
     </>
   );
 }
