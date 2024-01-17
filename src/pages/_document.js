a8eed6b48937cf844f131cc28f6bf8e3c97dc7b9
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-17 09:57:13 -0300

    favicon, meta tag autor idioma

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
+        <meta name="author" content="Melissa" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
