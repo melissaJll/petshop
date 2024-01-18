1307bf4b7199438a98ffaa2b40dc37465feb74bb
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-18 09:48:29 -0300

    Update Cabecalho.jsx

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -5,40 +5,56 @@ import styled from "styled-components";
 
 export default function Cabecalho() {
   return (
-    <>
-      <StyledHeader>
-        <div className="limitador">
-          <h1>
-            <Link href="/">
-              <Image
-                src="/images/logo.png"
-                width={48}
-                height={48}
-                alt="patinha dentro de um coração"
-              />
-              Petshop
-            </Link>
-          </h1>
-          <Menu></Menu>
-        </div>
-      </StyledHeader>
-    </>
+    <StyledHeader>
+      <div className="limitador">
+        <h1>
+          <Link href="/">
+            <Image
+              src="/images/logo.png"
+              width={48}
+              height={48}
+              alt="Patinha dentro de um coração"
+            />
+            PetShop
+          </Link>
+        </h1>
+        <Menu />
+      </div>
+    </StyledHeader>
   );
 }
 
 const StyledHeader = styled.header`
-  background-color: #faf9fc;
+  background-color: #f7f7f7;
   box-shadow: var(--sombra-box);
+
   h1 a {
     text-decoration: none;
     color: var(--cor-primaria);
     background-color: var(--cor-primaria-fundo);
-    /* #5b2081 */
-    border-radius: var(--borda-arredondada);
     padding: 0.5rem 1.2rem;
+    border-radius: var(--borda-arredondada);
     font-size: 1.5rem;
     display: flex;
     justify-content: center;
     align-items: center;
+    &:hover,
+    &:focus {
+      color: var(--cor-secundaria-hover);
+    }
+  }
+
+  img {
+    margin-right: 0.5rem;
+  }
+  /* pega tudo que está dentro da tag Header incluindo a div limitador*/
+  /* #5b2081 */
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    padding-bottom: 1rem;
   }
 `;
