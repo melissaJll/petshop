7f76806755800c32738fc8c7b3ac7668a24d983b
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-18 09:23:34 -0300

    StyledNav

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -1,14 +1,29 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Menu() {
   return (
     <>
-      <nav>
+      <StyledNav>
         <Link href="/">Blog</Link>
         <Link href="/produtos">Produtos</Link>
         <Link href="/sobre">Sobre</Link>
         <Link href="/contato">Contato</Link>
-      </nav>
+      </StyledNav>
     </>
   );
 }
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: var(--borda-arredondada);
+
+  a {
+    text-decoration: none;
+    color: var(--cor-primaria);
+    background-color: var(--cor-primaria-fundo);
+    padding: 0%.8rem 1rem;
+  }
+`;
