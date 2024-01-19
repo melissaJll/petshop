b2fdf4a319c0462abc4e1718faabd45028ce95dc
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-19 09:45:20 -0300

    Estilização do formulário

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -1,3 +1,4 @@
+import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
 
@@ -10,6 +11,31 @@ export default function Contato() {
       </Head>
       <StyledContato>
         <h2>Fale conosco</h2>
+        <Container>
+          <form action="" method="post">
+            <div>
+              <label htmlFor="nome">Nome: </label>
+              <input type="text" name="nome" id="nome" />
+            </div>
+            <div>
+              <label htmlFor="email">Email: </label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div>
+              <label htmlFor="mensagem">Mensagem: </label>
+              <textarea
+                name="mensagem"
+                id="mensagem"
+                cols="30"
+                rows="10"
+                maxLength={500}
+              ></textarea>
+            </div>
+            <div>
+              <button type="submit"> Enviar mensagem</button>
+            </div>
+          </form>
+        </Container>
       </StyledContato>
     </>
   );
@@ -18,4 +44,39 @@ const StyledContato = styled.section`
   h2::before {
     content: "💌 ";
   }
+  button {
+    background-color: #663a83;
+    border: none;
+    color: aliceblue;
+    padding: 0.8rem;
+    border-radius: var(--borda-arredondada);
+    margin: 1.3rem auto;
+    font-weight: bold;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #faf7fc;
+    color: #041047;
+    font-size: 1.1rem;
+  }
+  form {
+    width: 95%;
+    margin: auto;
+  }
+  input,
+  textarea {
+    width: 71%;
+    margin: 0.6rem;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 4px 4px 10px 1px rgb(0 0 0 / 10%);
+  }
+  input {
+    padding: 0.8rem;
+  }
+  /* label::before {
+    content: "🦴 ";
+  } */
 `;
