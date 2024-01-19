5ace3faf786d6d2591d0c125ded26065305d24ca
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-19 10:39:42 -0300

    responsividade dos posts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,6 +30,20 @@ export default function Home() {
               <p>Subtitulo</p>
             </Link>
           </article>
+
+          <article>
+            <Link href="">
+              <h3>Titulo...</h3>
+              <p>Subtitulo</p>
+            </Link>
+          </article>
+
+          <article>
+            <Link href="">
+              <h3>Titulo...</h3>
+              <p>Subtitulo</p>
+            </Link>
+          </article>
         </StyledListaPosts>
       </StyledHome>
     </>
@@ -57,6 +71,16 @@ const StyledListaPosts = styled.div`
   article:hover {
     cursor: pointer;
     transform: scale(1.05);
+  }
+
+  @media screen and (min-width: 500px) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    article {
+      width: 49%;
+    }
   }
 `;
 
