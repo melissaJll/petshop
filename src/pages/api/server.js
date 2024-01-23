c0a2e81854f36ba863adefc50d9bbdb4b936396c
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-23 10:51:41 -0300

    correção

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,6 +1,6 @@
 const porta = 2112;
 const endereco = "10.20.46.28";
 
-const serverApi = `https://${endereco}:${porta}`;
+const serverApi = `http://${endereco}:${porta}`;
 
 export default serverApi;
