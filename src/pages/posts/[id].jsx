419f1b7089249156de4e844aa69d875ebee596c6
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-23 11:05:17 -0300

    estrutura da página

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,23 @@
+import Container from "@/components/ui/Container";
+import Head from "next/head";
+import styled from "styled-components";
+
 export default function Post() {
-  return <h2>Página Post</h2>;
+  return (
+    <>
+      <Head>
+        <title>Titulo - Petshop</title>
+        <meta name="description" content="Descrição" />
+      </Head>
+
+      <StyledPost>
+        <h2>Titulo</h2>
+        <Container>
+          <h3>Categoria</h3>
+          <p>Descricao</p>
+        </Container>
+      </StyledPost>
+    </>
+  );
 }
+const StyledPost = styled.article``;
