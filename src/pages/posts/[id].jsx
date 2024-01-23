14ffbbeda6812ae75634e622523364c5fc56ed04
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-23 11:32:56 -0300

    configuração do getStaticProps

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,6 +1,7 @@
 import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
+import serverApi from "../api/server";
 
 export async function getStaticPaths() {
   return {
@@ -10,10 +11,28 @@ export async function getStaticPaths() {
 }
 
 // getStaticProps()
-// export async function getStaticProps({ params }) {
-//   const { id } = params;
-//   console.log(id);
-// }
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  console.log(id);
+
+  try {
+    const resposta = await fetch(`${serverApi}/posts/${id}`);
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+
+    const dados = await resposta.json();
+
+    return {
+      props: {
+        post: dados,
+      },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+  }
+}
 
 export default function Post() {
   return (
