fafa17cbc150a7f154978c92d5966143141f7a87
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-24 09:57:24 -0300

    botôes com as categorias

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,23 +9,19 @@ export async function getStaticProps() {
   try {
     const resposta = await fetch(`${serverApi}/posts`);
     const dados = await resposta.json();
-    // console.log(dados); daqui vem CATEGORIA
 
     if (!resposta.ok) {
       throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
     }
 
-    /*Extraindo as categorias dos posts para um novo array com map*/
-    // dados é um array coms os objetos post
+    /* Extraindo as categorias dos posts para um novo array */
     const categorias = dados.map((post) => post.categoria);
     console.log(categorias);
 
-    /* gerando um array de categorias sem repetição */
+    /* Gerando um array de categorias ÚNICAS */
     const categoriasUnicas = [...new Set(categorias)];
     console.log(categoriasUnicas);
 
-    // objeto dentro de objeto - props recebe dados
-    //Após o processamento (desde de que nao haja erros ), a getStaticProps retorna um objeto com uma propriedade chamada "props"
     return {
       props: {
         posts: dados,
@@ -57,6 +53,13 @@ export default function Home({ posts, categorias }) {
       {/* Definindo prop  = como o resultado do state setListaDePosts(dados) = listaDePosts */}
       <StyledHome>
         <h2>Pet noticias</h2>
+
+        <div>
+          {categorias.map((categoria) => {
+            return <button>{categoria}</button>;
+          })}
+        </div>
+
         <ListaPost posts={listaDePosts} />
       </StyledHome>
     </>
