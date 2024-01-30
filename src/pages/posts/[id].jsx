6671408a132904ba80c23523a7a31ba9e6071b01
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-30 11:02:16 -0300

    Servidor local não é mais necessário apenas Firebase

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -8,7 +8,7 @@ export async function getStaticProps({ params }) {
   console.log(id);
 
   try {
-    const resposta = await fetch(`${serverApi}/posts/${id}`);
+    const resposta = await fetch(`${serverApi}/posts/${id}.json`);
 
     if (!resposta.ok) {
       throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
