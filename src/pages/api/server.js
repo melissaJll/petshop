76e85782c9973aae99ec603f4505634415a74998
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-30 09:36:11 -0300

    mudando endereço da api

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,7 +1,7 @@
-const porta = 2112;
-const endereco = "10.20.46.28";
+// const porta = 2112;
+// const endereco = "10.20.46.28";
 // 10.20.47.34
 
-const serverApi = `http://${endereco}:${porta}`;
+const serverApi = `https://petshop-melissa-default-rtdb.firebaseio.com/`;
 
 export default serverApi;
